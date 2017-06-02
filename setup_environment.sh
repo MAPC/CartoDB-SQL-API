e3d701fab39e8fb66e8ba32397c919e057f84370
allthesignals <wilburnforce@gmail.com>
2017-06-01 22:26:42 -0400

    Create special heroku production sample file
    Since environment variables are being written to the document, a special heroku-specific config file needs to be made, which includes variable names.

--- setup_environment.sh
+++ setup_environment.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cp config/environments/docker.js config/environments/$CARTO_ENV.js
+cp config/environments/heroku_production.js.example config/environments/$CARTO_ENV.js
 
 sed -i "s/__DB_PORT__/$DB_PORT/g" config/environments/$CARTO_ENV.js
 sed -i "s/__DB_HOST__/$DB_HOST/g" config/environments/$CARTO_ENV.js
@@ -9,5 +9,3 @@ sed -i "s/__REDIS_PORT__/$REDIS_PORT/g" config/environments/$CARTO_ENV.js
 sed -i "s/__CARTO_ENV__/$CARTO_ENV/g" config/environments/$CARTO_ENV.js
 sed -i "s^__CARTO_DB_PREFIX__^$CARTO_DB_PREFIX^g" config/environments/$CARTO_ENV.js
 sed -i "s^__CARTO_DB_USER_PREFIX__^$CARTO_DB_USER_PREFIX^g" config/environments/$CARTO_ENV.js
-
-node app.js $CARTO_ENV
